2afab905089cddcaeb9e8a6424862d813883a60d
Graphenpraktikum <grpraktikum@prak04.bioinf.uni-leipzig.de>
2016-02-02 14:43:58 +0100

    update

--- catalan/run.py
+++ catalan/run.py
@@ -1,20 +1,37 @@
 include("catalanRules.py")
 include("catalanLevels.py")
 postSection("Input Graphs")
-for a in inputGraphs:
-	a.print()
-postSection("Input Rules")
-for a in inputRules:
-	a.print()
 
-# strat = (
-# 	addUniverse(formaldehyde)
-# 	>> addSubset(glycolaldehyde)
-# 	# iterate the rule application 4 times
-# 	>> repeat[4](
-# 		inputRules
-# 	)
-# )
-# dg = dgRuleComp(inputGraphs, strat)
-# dg.calc()
-# dg.print()
+#SIMPLE_BLOSSOM.print()
+# LANDSCAPE.print()
+# for a in inputGraphs:
+#         a.print()
+
+# postSection("Input Rules")
+# for a in inputRules:
+#  	a.print()
+
+strat0 = (
+        addUniverse(SIMPLE_BLOSSOM)
+        # >> 
+        #addSubset(SIMPLE_BLOSSOM)
+        # >> repeat[1] (
+        >> mark
+        # )        
+)
+
+strat = (
+        #addUniverse(SIMPLE_BLOSSOM)
+        #>> 
+        addSubset(SIMPLE_BLOSSOM)
+        >> mark
+        >> revive(
+                markForFail
+        )
+        >> unmark
+)
+
+dg = dgRuleComp(inputGraphs, strat0)
+dg.calc()
+dg.print()
+
