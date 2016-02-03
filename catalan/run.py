83e03a499131b2336725ef64a710a719ac1acfa5
Graphenpraktikum <grpraktikum@prak04.bioinf.uni-leipzig.de>
2016-02-03 14:55:00 +0100

    automated commit

--- catalan/run.py
+++ catalan/run.py
@@ -4,57 +4,44 @@ include("catalanLevels.py")
 # for a in inputGraphs:
 #         a.print()
 
-postSection("Input Rules")
-for a in inputRules:
- 	a.print()
+# postSection("Input Rules")
+# for a in inputRules:
+#  	a.print()
 
-# LEVEL = FIRST_LEVEL
-# LEVEL = SIMPLE_BLOSSOM
-# LEVEL = HONEY_SUCKLE
-LEVEL = DRAGON
+#LEVEL = FIRST_LEVEL
+LEVEL = SIMPLE_BLOSSOM
+#LEVEL = HONEY_SUCKLE
+#LEVEL = DRAGON
+#LEVEL = TREE
+postSection("Catalan level to solve")
+LEVEL.print()
+
 strat1 = (
-    #addUniverse(LEVEL)
-    #>> 
-    addSubset(LEVEL)
+    addUniverse(LEVEL)
+    >> addSubset(LEVEL)
     >> repeat(
         mark
-        >> repeat(reattachExternal)
-        >> repeat(removeAttached)
-        >> repeat(removeInterR)
+        >> repeat(revive(reattachExternal))
+        >> repeat(revive(removeAttached))
+        >> repeat(revive(removeInterR))
         >> removeR
         >> unmark
     )
-)
-
-test = (
-    addUniverse(LEVEL)
-    >> addSubset(LEVEL)
-    >> mark   
-    >> rightPredicate[
-        lambda d: all(a.vLabelCount("FAIL") == 0 for a in d.right)
-    ](
-        markForFail
-        # repeat(removeAttached)
-        # >> repeat(reattachExternal)
-        # >> repeat(removeInterR)
-    )
-    >> unmark
-    # >> markForFail
-    # >> leftPredicate[
-    #     lambda d: all(a.vLabelCount("FAIL") == 0 for a in d.left)
-    # ](
-    #     test
-    #     # repeat(removeAttached)
-    #     # >> repeat(reattachExternal)
-    #     # >> repeat(removeInterR)
-    # )
-    # >> removeR
-    # >> unmark
 )
 
 strat = strat1
 
 dg = dgRuleComp(inputGraphs, strat)
 dg.calc()
-dg.print()
+#dg.print()
+dgPrinter = DGPrinter()
+dgPrinter.withGraphImages =False # be careful, the derivation graphs can become quite large
+
+dg.print(dgPrinter)
+
+if SUCCESS in dg.vertices:
+    print("SUCCESS")
+else:
+    print("no solution found")
+
 
