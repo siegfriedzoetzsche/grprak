b3a274f62f5d18790422d687e43268bbb9e98ee3
Graphenpraktikum <grpraktikum@prak04.bioinf.uni-leipzig.de>
2016-02-04 12:26:19 +0100

    automated commit

--- catalan/build.py
+++ catalan/build.py
@@ -11,11 +11,26 @@ include("catalanLevels.py")
 LEVEL = SUCCESS
 
 strat1 = (
+    # add just one node with label "0"
     addUniverse(LEVEL)
     >> addSubset(LEVEL)
-    >> addThreeNodes
-    >> repeat(revive(addEdge))
-    >> revive(addThreeNodes)
+    # expand a node with vertex degree 0
+    >> dZeroExpand
+    # add 0, 1, 2 or 3 edges beween the arms of the star
+    >> {
+        addZeroInterREdges,
+        addOneInterREdge,
+        addTwoInterREdges,
+        addThreeInterREdges,
+    }
+    # expand further
+    >> {dOneExpand, dTwoExpandOne}
+    >> {
+        addZeroInterREdges,
+        addOneInterREdge,
+        addTwoInterREdges,
+        addThreeInterREdges,
+    }
 )
 
 strat = strat1
@@ -27,20 +42,20 @@ dgPrinter = DGPrinter()
 dgPrinter.withGraphImages = True # False # be careful, the derivation graphs can become quite large
 dg.print(dgPrinter)
 
-flowPrinter = DGFlowPrinter()
-flowPrinter.printUnfiltered = False
-flow = dgFlow(dg)
-flow.objectiveFunction = DGFlowLinExp() # important, otherwise the default function will min. #edgesUsed which may take a long time
-flow.addSource(LEVEL)
-flow.addSink(SUCCESS)
-flow.addConstraint(inFlow(LEVEL) == 1)
-flow.calc()
-#flow.solutions.print(flowPrinter)
+# flowPrinter = DGFlowPrinter()
+# flowPrinter.printUnfiltered = False
+# flow = dgFlow(dg)
+# flow.objectiveFunction = DGFlowLinExp() # important, otherwise the default function will min. #edgesUsed which may take a long time
+# flow.addSource(LEVEL)
+# flow.addSink(SUCCESS)
+# flow.addConstraint(inFlow(LEVEL) == 1)
+# flow.calc()
+# #flow.solutions.print(flowPrinter)
 
-if SUCCESS in dg.vertices:
-    flow.solutions.print(flowPrinter)
-    print("SUCCESS")
-else:
-    print("no solution found")
+# if SUCCESS in dg.vertices:
+#     flow.solutions.print(flowPrinter)
+#     print("SUCCESS")
+# else:
+#     print("no solution found")
 
 
--- catalan/catalanBuildRules.py
+++ catalan/catalanBuildRules.py
@@ -1,37 +1,196 @@
-addThreeNodes = ruleGMLString("""
+#############
+# d(v) == 0 #
+#############
+dZeroExpand = ruleGMLString("""
 rule[
-    ruleID "addThreeNodes"
+    ruleID "dZeroExpand"
     left[
-    ]
-    context[
         node[id 0 label "0"]
     ]
+    context[
+    ]
     right[
-        node[id 1 label "0"]
-        node[id 2 label "0"]
-        node[id 3 label "0"]
-        edge[source 0 target 1 label""]
-        edge[source 0 target 2 label""]
-        edge[source 0 target 3 label""]
+        node[id 0 label "A"]
+        node[id 1 label "R"]
+        node[id 2 label "R"]
+        node[id 3 label "R"]
+        edge[source 0 target 1 label ""]
+        edge[source 0 target 2 label ""]
+        edge[source 0 target 3 label ""]
+    ]
+    constrainAdj[
+        id 0 op = count 0
     ]
 ]
 """)
 
-addEdge = ruleGMLString("""
+#############
+# d(v) == 1 #
+#############
+dOneExpand = ruleGMLString("""
 rule[
-    ruleID "addEdge"
+    ruleID "dZeroExpand"
     left[
+        node[id 0 label "0"]
+        edge[source -1 target 0 label ""]
     ]
     context[
+        node[id -1 label "0"]
+    ]
+    right[
+        node[id 0 label "A"]
+        node[id 1 label "R"]
+        node[id 2 label "R"]
+        node[id 3 label "R"]
+
+        edge[source -1 target 1 label ""]
+
+        edge[source 0 target 1 label ""]
+        edge[source 0 target 2 label ""]
+        edge[source 0 target 3 label ""]
+    ]
+    constrainAdj[
+        id 0 op = count 1
+    ]
+]
+""")
+
+#############
+# d(v) == 2 #
+#############
+dTwoExpandOne = ruleGMLString("""
+rule[
+    ruleID "dTwoExpandOne"
+    left[
+        node[id 0 label "0"]
+        edge[source -1 target 0 label ""]
+        edge[source -2 target 0 label ""]
+    ]
+    context[
+        node[id -1 label "0"]
+        node[id -2 label "0"]
+    ]
+    right[
+        node[id 0 label "A"]
+        node[id 1 label "R"]
+        node[id 2 label "R"]
+        node[id 3 label "R"]
+
+        edge[source -1 target 1 label ""]
+        edge[source -2 target 1 label ""]
+
+        edge[source 0 target 1 label ""]
+        edge[source 0 target 2 label ""]
+        edge[source 0 target 3 label ""]
+    ]
+    constrainAdj[
+        id 0 op = count 2
+    ]
+]
+""")
+
+##################
+# add star edges #
+##################
+
+addZeroInterREdges = ruleGMLString("""
+rule[
+    ruleID "addZeroInterREdges"
+    left[
+        node[id 0 label "A"]
+        node[id 1 label "R"]
+        node[id 2 label "R"]
+        node[id 3 label "R"]
+    ]
+    context[
+        edge[source 0 target 1 label ""]
+        edge[source 0 target 2 label ""]
+        edge[source 0 target 3 label ""]
+    ]
+    right[
         node[id 0 label "0"]
         node[id 1 label "0"]
         node[id 2 label "0"]
-        edge[source 0 target 1 label""]
-        edge[source 0 target 2 label""]        
+        node[id 3 label "0"]
     ]
-    right[
-        edge[source 1 target 2 label""]
-    ]
-    #constrainNoEdge[source 1 target 2]
 ]
 """)
+
+addOneInterREdge = ruleGMLString("""
+rule[
+    ruleID "addOneInterREdge"
+    left[
+        node[id 0 label "A"]
+        node[id 1 label "R"]
+        node[id 2 label "R"]
+        node[id 3 label "R"]
+    ]
+    context[
+        edge[source 0 target 1 label ""]
+        edge[source 0 target 2 label ""]
+        edge[source 0 target 3 label ""]
+    ]
+    right[
+        node[id 0 label "0"]
+        node[id 1 label "0"]
+        node[id 2 label "0"]
+        node[id 3 label "0"]
+
+        edge[source 1 target 2 label ""]
+    ]
+]
+""")
+
+addTwoInterREdges = ruleGMLString("""
+rule[
+    ruleID "addTwoInterREdges"
+    left[
+        node[id 0 label "A"]
+        node[id 1 label "R"]
+        node[id 2 label "R"]
+        node[id 3 label "R"]
+    ]
+    context[
+        edge[source 0 target 1 label ""]
+        edge[source 0 target 2 label ""]
+        edge[source 0 target 3 label ""]
+    ]
+    right[
+        node[id 0 label "0"]
+        node[id 1 label "0"]
+        node[id 2 label "0"]
+        node[id 3 label "0"]
+
+        edge[source 1 target 2 label ""]
+        edge[source 1 target 3 label ""]
+    ]
+]
+""")
+
+addThreeInterREdges = ruleGMLString("""
+rule[
+    ruleID "addThreeInterREdges"
+    left[
+        node[id 0 label "A"]
+        node[id 1 label "R"]
+        node[id 2 label "R"]
+        node[id 3 label "R"]
+    ]
+    context[
+        edge[source 0 target 1 label ""]
+        edge[source 0 target 2 label ""]
+        edge[source 0 target 3 label ""]
+    ]
+    right[
+        node[id 0 label "0"]
+        node[id 1 label "0"]
+        node[id 2 label "0"]
+        node[id 3 label "0"]
+
+        edge[source 1 target 2 label ""]
+        edge[source 1 target 3 label ""]
+        edge[source 2 target 3 label ""]
+    ]
+]
+""")
+
